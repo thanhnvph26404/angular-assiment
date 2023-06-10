e331821d819c1ec76b351458e866582081ec84f1
thanhnvph26404 <thanhnvph26404@gmail.com>
2023-06-10 10:50:36 +0700

    feat: [BE] signin
    refs: AA-23

--- server/src/schemas/auth.js
+++ server/src/schemas/auth.js
@@ -21,3 +21,16 @@ export const signupSchema = joi.object({
         "any.required": `"confirmPassword" là trường bắt buộc`,
     }),
 });
+
+export const signinSchema = joi.object({
+    email: joi.string().email().required().messages({
+        "string.empty": 'Trường "email" không được để trống',
+        "string.email": 'Trường "email" không đúng định dạng',
+        "any.required": 'Trường "email" là bắt buộc',
+    }),
+    password: joi.string().min(6).required().messages({
+        "string.empty": 'Trường "mật khẩu" không được để trống',
+        "string.min": 'Trường "mật khẩu" phải có ít nhất 6 ký tự',
+        "any.required": "Trường mật khẩu là bắt buộc",
+    }),
+});
